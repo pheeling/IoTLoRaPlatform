1b5dac272e4f83a3b8fb1b9bac62f2767f131aba
Philipp Hiestand <hiestand@dinotronic.ch>
2021-03-26 00:17:58 +0100

    merge folders

---
+++ index.js
@@ -0,0 +1,64 @@
+// Require the client library packages
+const Iota = require('@iota/core');
+const Converter = require('@iota/converter');
+const got = require('got');
+
+// Create a new instance of the IOTA API object
+// Use the `provider` field to specify which node to connect to
+const iota = Iota.composeAPI({
+provider: 'https://nodes.devnet.iota.org:443'
+});
+
+const depth = 3;
+const minimumWeightMagnitude = 9;
+
+const address =
+'HEQLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWOR99D';
+
+const seed =
+'PUEOTSEITFEVEWCWBTSIZM9NKRGJEIMXTULBACGFRQK9IMGICLBKW9TTEVSDQMGWKBXPVCBMMCXWMNPDX';
+
+var message = ''
+
+var options = {
+    'method': 'GET',
+    'url': 'http://myStrom-Switch-43F2B8/report',
+    'headers': {
+    }
+};
+
+(async () => {
+	try {
+		const response = await got(options);
+		console.log('statusCode:', response.statusCode);
+        message = response.body
+		console.log(message);
+
+	} catch (error) {
+		console.log('error:', error);
+	}
+})();
+
+const messageInTrytes = Converter.asciiToTrytes(message);
+
+const PrepareTransfers = Iota.createPrepareTransfers();
+const transfers = [
+    {
+        value: 0,
+        address: address,
+        message: messageInTrytes
+    }
+];
+
+Iota
+    PrepareTransfers(seed, transfers)
+    .then(trytes => {
+        return iota.sendTrytes(trytes, depth, minimumWeightMagnitude);
+    })
+    .then(bundle => {
+        console.log(bundle[0].hash)
+    })
+    .catch(err => {
+        console.error(err)
+    });
+
