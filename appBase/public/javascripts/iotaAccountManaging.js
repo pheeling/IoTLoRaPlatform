1d13d4604775e1666f5e32d12dec17c2bd69eec1
Philipp Hiestand <hiestand@dinotronic.ch>
2021-11-08 23:42:32 +0100

    saving hash

---
+++ appBase/public/javascripts/iotaAccountManaging.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcryptjs');
+const fs = require('fs');
+const util = require('util');
+const filepathEnvFile = 'config/.env'
+const envfile = require('envfile')
+
+// Convert fs.readFile / writefile into Promise version of same    
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+const appendFile = util.promisify(fs.appendFile);
+
+const envParseFile = util.promisify(envfile.parse)
+const envStringifyFile = util.promisify(envfile.stringify)
+
+function savePassword(PlainTextPassword){
+    bcrypt.hash(PlainTextPassword, saltRounds, function(err, hash) {
+        try {
+            //https://stackoverflow.com/questions/53360535/how-to-save-changes-in-env-file-in-node-js
+            let parsedFile = await envParseFile(filepathEnvFile);
+            parsedFile.SH_PASSWORD = hash
+            writeFile(filepathEnvFile, envStringifyFile(parsedFile))
+            return "saved hash"
+          }
+          catch (err){
+            console.log(err)
+          }
+    });
+}
+
+function compareHash(PlainTextPassword){
+    bcrypt.compare(PlainTextPassword, hash, function(err, result) {
+        return result
+    });
+}
