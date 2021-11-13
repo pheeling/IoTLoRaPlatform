dc8581c786293416058aac767ef3523b829a0554
Philipp Hiestand <hiestand@dinotronic.ch>
2021-11-14 00:39:14 +0100

    env File writing ok, reading open

--- appBase/public/javascripts/iotaAccountManaging.js
+++ appBase/public/javascripts/iotaAccountManaging.js
@@ -2,7 +2,9 @@ const bcrypt = require('bcryptjs');
 const fs = require('fs');
 const util = require('util');
 const filepathEnvFile = 'config/.env'
-const envfile = require('envfile')
+const envfile = require('envfile');
+const { runInContext } = require('vm');
+const saltRounds = 10;
 
 // Convert fs.readFile / writefile into Promise version of same    
 const readFile = util.promisify(fs.readFile);
@@ -10,16 +12,15 @@ const writeFile = util.promisify(fs.writeFile);
 const appendFile = util.promisify(fs.appendFile);
 
 const envParseFile = util.promisify(envfile.parse)
-const envStringifyFile = util.promisify(envfile.stringify)
 
 function savePassword(PlainTextPassword){
     bcrypt.hash(PlainTextPassword, saltRounds, function(err, hash) {
         try {
             //https://stackoverflow.com/questions/53360535/how-to-save-changes-in-env-file-in-node-js
-            let parsedFile = await envParseFile(filepathEnvFile);
+            let parsedFile = envParseFile(filepathEnvFile);
             parsedFile.SH_PASSWORD = hash
-            writeFile(filepathEnvFile, envStringifyFile(parsedFile))
-            return "saved hash"
+            writeFile(filepathEnvFile, envfile.stringify(parsedFile))
+            console.log("saved hash")
           }
           catch (err){
             console.log(err)
@@ -27,9 +28,18 @@ function savePassword(PlainTextPassword){
     });
 }
 
-function compareHash(PlainTextPassword){
-    //TODO Read .env File and compare password
+async function compareHash(PlainTextPassword){
+    // Test comparison 
+    var parsedFile = await envParseFile(filepathEnvFile);
+    var hash = parsedFile.SH_PASSWORD
     bcrypt.compare(PlainTextPassword, hash, function(err, result) {
+        console.log(result)
         return result
     });
+
 }
+
+module.exports = { compareHash, savePassword}
+
+// savePassword("test")
+compareHash("test")
