28004adbab7c111f25691424d242989af790d27e
Philipp Hiestand <hiestand@dinotronic.ch>
2021-11-14 22:59:28 +0100

    db and account creation

--- appBase/public/javascripts/iotaAccountManaging.js
+++ appBase/public/javascripts/iotaAccountManaging.js
@@ -3,15 +3,73 @@ const fs = require('fs');
 const util = require('util');
 const filepathEnvFile = 'config/.env'
 const envfile = require('envfile');
-const { runInContext } = require('vm');
 const saltRounds = 10;
 
-// Convert fs.readFile / writefile into Promise version of same    
+// Convert fs.readFile, fs.writeFile, envfile.parse / readfile,writefile,envParseFile into Promise version of same    
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 const appendFile = util.promisify(fs.appendFile);
-
 const envParseFile = util.promisify(envfile.parse)
+
+// iota Account Manager dependency
+require('dotenv').config();
+const filepathIotaData = 'config/'
+
+require('dotenv').config();
+
+async function createDB(dbname,password) {
+    const { AccountManager } = require('@iota/wallet');
+ 
+    const manager = new AccountManager({
+        storagePath: filepathIotaData + dbname + '-database',
+    });
+    let result = await compareHash(password)
+    if(result == true){
+        try {
+            manager.setStrongholdPassword(password);
+        } catch (error) {
+            console.log('Error: ' + error);
+        }
+    }
+}
+
+async function createAccount(dbname,accountName,password){
+    const { AccountManager } = require('@iota/wallet');
+ 
+    const manager = new AccountManager({
+        storagePath: filepathIotaData + dbname + '-database',
+    });
+
+    let result = await compareHash(password)
+    if(result == true){
+        try {
+            manager.setStrongholdPassword(password);
+            let account;
+            try {
+                account = manager.getAccount(accountName);
+            } catch (e) {
+                console.log("Couldn't get account, creating a new one");
+            }
+
+            if (!account) {
+                manager.storeMnemonic(SignerType.Stronghold);
+                account = manager.createAccount({
+                    clientOptions: {
+                        node: { url: 'https://api.lb-0.h.chrysalis-devnet.iota.cafe' },
+                        localPow: true,
+                    },
+                    alias: accountName,
+                });
+                console.log('Account created:', account.id());
+
+                const synced = await account.sync();
+                console.log('Synced account', synced);
+            }
+        } catch (error) {
+            console.log('Error: ' + error);
+        }
+    }
+}
 
 function savePassword(PlainTextPassword){
     bcrypt.hash(PlainTextPassword, saltRounds, function(err, hash) {
@@ -29,17 +87,13 @@ function savePassword(PlainTextPassword){
 }
 
 async function compareHash(PlainTextPassword){
-    // Test comparison 
-    var parsedFile = await envParseFile(filepathEnvFile);
-    var hash = parsedFile.SH_PASSWORD
-    bcrypt.compare(PlainTextPassword, hash, function(err, result) {
+    let filecontent = await readFile(filepathEnvFile)
+    let parsedFile = envfile.parse(filecontent)
+    bcrypt.compare(PlainTextPassword, parsedFile.SH_PASSWORD, function(err, result) {
         console.log(result)
         return result
     });
 
 }
 
-module.exports = { compareHash, savePassword}
-
-// savePassword("test")
-compareHash("test")
+module.exports = { compareHash, savePassword, createDB, createAccount}
